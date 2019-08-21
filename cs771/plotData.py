b4a71c2b55985d67660cdc54743619d0d9acaef9
Puru <purushot@cse.iitk.ac.in>
2019-08-21 19:54:05 +0530

    Added batch mode for plotting curves as well

--- cs771/plotData.py
+++ cs771/plotData.py
@@ -21,13 +21,18 @@ def getFigure( sizex = 7, sizey = 7 ):
     fig = plt.figure( figsize = (sizex, sizey) )
     return fig
 
-def plotCurve( responseGenerator, fig, mode = 'point', color = 'b', line = 'solid', xlimL = 0, xlimR = 10, nBins = 500 ):
+def plotCurve( responseGenerator, fig, mode = "point", color = 'b', line = "solid", xlimL = 0, xlimR = 10, nBins = 500, label = "" ):
     X = np.linspace( xlimL, xlimR, nBins, endpoint = True )
-    y = np.zeros( X.shape )
-    for i in range( X.size ):
-        y[i] = responseGenerator( X[i] )
+    if mode == "point":
+	    y = np.zeros( X.shape )
+	    for i in range( X.size ):
+		    y[i] = responseGenerator( X[i] )
+    elif mode == "batch":
+	    y = responseGenerator( X )
     plt.figure( fig.number )
-    plt.plot( X, y, color = color, linestyle = line )
+    plt.plot( X, y, color = color, linestyle = line, label = label )
+    if label:
+        plt.legend()
 
 def plot2D( X, fig, color = 'r', marker = '+', size = 100 ):
     plt.figure( fig.number )
@@ -37,14 +42,14 @@ def plot2DPoint( X, fig, color = 'r', marker = '+', size = 100 ):
     plt.figure( fig.number )
     plt.scatter( X[0], X[1], s = size, c = color, marker = marker )
 
-def shade2D( labelGenerator, fig, mode = 'point', colorMap = lrlg, xlim = 10, ylim = 10, nBins = 500 ):
+def shade2D( labelGenerator, fig, mode = "point", colorMap = lrlg, xlim = 10, ylim = 10, nBins = 500 ):
     xi, yi = np.mgrid[ -xlim:xlim:nBins*1j, -ylim:ylim:nBins*1j ]
     zi = np.zeros( xi.shape )
-    if mode == 'point':
+    if mode == "point":
         for i in range( zi.shape[0] ):
             for j in range( zi.shape[1] ):
                 zi[i,j] = labelGenerator( xi[i,j], yi[i,j] )
-    elif mode == 'batch':
+    elif mode == "batch":
         zi = labelGenerator( np.hstack( (xi.reshape((xi.size, 1)), yi.reshape((yi.size, 1))) ) )
         zi = zi.reshape( xi.shape )
     zi[zi < 1/nBins] = 0
